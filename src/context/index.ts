f71fbceecc0b020957ce22d0ccb071f638333c10
Bryan Garzon <bryan.garzon@ticketmaster.com>
2021-02-27 12:39:26 -0500

    Step4 - currying our produce functions

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,32 +1,30 @@
 import produce from "immer";
 import { State } from "../types";
 
-export function addGift(
-  state: State,
-  id: string,
-  description: string,
-  image: string
-) {
-  return produce(state, (draft) => {
+export const addGift = produce(
+  (draft: State, id: string, description: string, image: string) => {
     draft.gifts.push({
       id,
       description,
       image,
       reservedBy: undefined,
     });
-  });
-}
+  }
+);
 
-export function toggleReservation(state: State, giftId: string) {
-  return produce(state, (draft) => {
-    const gift = draft.gifts.find((gift) => gift.id === giftId);
-    if (gift) {
-      gift.reservedBy =
-        gift.reservedBy === undefined
-          ? state.currentUser.id
-          : gift.reservedBy === state.currentUser.id
-          ? undefined
-          : gift.reservedBy;
-    }
-  });
-}
+export const toggleReservation = produce((draft: State, giftId: string) => {
+  const gift = draft.gifts.find((gift) => gift.id === giftId);
+  if (gift) {
+    gift.reservedBy =
+      gift.reservedBy === undefined
+        ? draft.currentUser.id
+        : gift.reservedBy === draft.currentUser.id
+        ? undefined
+        : gift.reservedBy;
+  }
+});
+
+// better produce using curry functions
+// produce(state, recipe) => nextState
+// produce(state) => (state) => nextState
+// defining them around the draft
